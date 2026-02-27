13b9bf0e1a98bb33267c48e7445ecde1a02ff00f
bryanbundang3 <bryanbundang3@gmail.com>
2026-02-27 19:38:44 +0800

    Add service worker for offline use

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
+const CACHE_NAME = 'panda-remit-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;600;700&display=swap'
+];
 
+// Install the Service Worker and cache the files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch files from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
